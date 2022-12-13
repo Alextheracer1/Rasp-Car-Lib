3f09c01d775cb6755113a0bef5fe75ecc1ac41eb
Alexander Graf <alex.ander.graf@icloud.com>
2022-12-13 13:28:38 +0100

    DistanceSensor implementation

--- src/main/java/org/alextheracer1/internal/components/DistanceSensor.java
+++ src/main/java/org/alextheracer1/internal/components/DistanceSensor.java
@@ -1,7 +1,103 @@
 package org.alextheracer1.internal.components;
 
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalInputConfig;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputConfig;
+import com.pi4j.io.gpio.digital.DigitalState;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class DistanceSensor {
 
+  private int PIN_TRIGGER;
+  private int PIN_ECHO;
+
+  public DistanceSensor(int PIN_TRIGGER, int PIN_ECHO) {
+    this.PIN_TRIGGER = PIN_TRIGGER;
+    this.PIN_ECHO = PIN_ECHO;
+  }
+
+  public int getPIN_TRIGGER() {
+    return PIN_TRIGGER;
+  }
+
+  public int getPIN_ECHO() {
+    return PIN_ECHO;
+  }
+
+  private DigitalInputConfig buildInputConfig(Context pi4j, int address) {
+    return DigitalInput.newConfigBuilder(pi4j)
+        .id("BCM" + address)
+        .name("DistanceSensorInput")
+        .address(address)
+        .provider("pigpio-digital-input")
+        .build();
+  }
+
+  private DigitalOutputConfig buildOutputConfig(Context pi4j, int address) {
+    return DigitalOutput.newConfigBuilder(pi4j)
+        .id("BCM" + address)
+        .name("DistanceSensorOutput")
+        .address(address)
+        .initial(DigitalState.LOW)
+        .shutdown(DigitalState.LOW)
+        .provider("pigpio-digital-output")
+        .build();
+  }
+
+
+  public float getDistance() throws InterruptedException {
+
+    var pi4j =  Pi4J.newAutoContext();
+
+
+    var output = pi4j.create(buildOutputConfig(pi4j, PIN_TRIGGER));
+
+    var input = pi4j.create(buildInputConfig(pi4j, PIN_ECHO));
+
+    AtomicLong unixTimeStart = new AtomicLong(-1);
+    AtomicLong unixTimeEnd = new AtomicLong(-1);
+
+    input.addListener(e -> {
+      if (e.state() == DigitalState.HIGH) {
+        unixTimeStart.set(System.currentTimeMillis());
+      } else if (e.state() == DigitalState.LOW){
+        if (unixTimeStart.get() != -1) {
+          unixTimeEnd.set(System.currentTimeMillis());
+        }
+      }
+    });
+
+
+    output.state(DigitalState.HIGH);
+    Thread.sleep(0, 1);
+    output.state(DigitalState.LOW);
+
+
+
+
+
+
+    while (unixTimeStart.get() == -1 || unixTimeEnd.get() == -1) {
+      Thread.sleep(500);
+    }
+
+    System.out.println(input.state());
+
+    float timeElapsed = unixTimeEnd.get() - unixTimeStart.get();
+    System.out.println("TimeElapsed: " + timeElapsed);
+
+    float distance = (timeElapsed * 34.3f) / 2f;
+
+    System.out.println("Distance: " + distance);
+
+    pi4j.shutdown();
+
+    return distance;
+
+  }
 
 
 }
--- src/test/java/DistanceSensorTest.java
+++ src/test/java/DistanceSensorTest.java
@@ -1,9 +1,11 @@
 import com.pi4j.Pi4J;
 import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.pi4j.io.gpio.digital.DigitalState;
 import java.util.concurrent.atomic.AtomicLong;
+import org.alextheracer1.internal.components.DistanceSensor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class DistanceSensorTest {
 
@@ -15,63 +17,10 @@ public class DistanceSensorTest {
 
     var pi4j =  Pi4J.newAutoContext();
 
+    DistanceSensor distanceSensor = new DistanceSensor(PIN_TRIGGER, PIN_ECHO);
 
+    Assertions.assertTrue(distanceSensor.getDistance() >= 0);
 
-
-    var config = DigitalOutput.newConfigBuilder(pi4j)
-        .address(PIN_TRIGGER)
-        .initial(DigitalState.LOW)
-        .shutdown(DigitalState.LOW)
-        .provider("pigpio-digital-output")
-        .build();
-
-    var output = pi4j.create(config);
-
-    var config2 = DigitalInput.newConfigBuilder(pi4j)
-        .address(PIN_ECHO)
-        .provider("pigpio-digital-input")
-        .build();
-
-    var input = pi4j.create(config2);
-
-
-    AtomicLong unixTimeStart = new AtomicLong(-1);
-    AtomicLong unixTimeEnd = new AtomicLong(-1);
-
-    input.addListener(e -> {
-      if (e.state() == DigitalState.HIGH) {
-        unixTimeStart.set(System.currentTimeMillis());
-      } else if (e.state() == DigitalState.LOW){
-        if (unixTimeStart.get() != -1) {
-          unixTimeEnd.set(System.currentTimeMillis());
-        }
-      }
-    });
-
-
-    output.state(DigitalState.HIGH);
-    Thread.sleep(0, 1);
-    output.state(DigitalState.LOW);
-
-
-
-
-
-
-    while (unixTimeStart.get() == -1 || unixTimeEnd.get() == -1) {
-      Thread.sleep(500);
-    }
-
-    System.out.println(input.state());
-
-    float timeElapsed = unixTimeEnd.get() - unixTimeStart.get();
-    System.out.println("TimeElapsed: " + timeElapsed);
-
-    float distance = (timeElapsed * 34.3f) / 2f;
-
-    System.out.println("Distance: " + distance);
-
-    pi4j.shutdown();
   }
 
 }
